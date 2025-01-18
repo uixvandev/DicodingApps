0ceeb6a61e259053ce359b23ae70cb56bbe6668b
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2025-01-18 23:35:44 +0700

    add navigation

---
+++ DicodingApps/AcademyModel.swift
@@ -0,0 +1,8 @@
+//
+//  AcademyModel.swift
+//  DicodingApps
+//
+//  Created by irfan wahendra on 14/01/25.
+//
+
+import Foundation
---
+++ DicodingApps/AcademyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AcademyTableViewCell.swift
+//  DicodingApps
+//
+//  Created by irfan wahendra on 14/01/25.
+//
+
+import UIKit
+
+class AcademyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
